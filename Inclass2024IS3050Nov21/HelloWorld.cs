654414158c16af3a5fd4cadf48cb3e218d10b1f9
Annapoorna Nair <annapoorna.nair21@gmail.com>
2024-11-21 11:20:02 -0500

    Adjusted the method

--- Inclass2024IS3050Nov21/HelloWorld.cs
+++ Inclass2024IS3050Nov21/HelloWorld.cs
@@ -8,6 +8,6 @@ namespace Inclass2024IS3050Nov21
     public class HelloWorld
     {
         public static void SayHello()
-            Console.WriteLine("Hello from HelloWorld(""))
+            Console.WriteLine("Hello from HelloWorld");
     }
 }
